def4d21ad3f89a2dbbee4386d5abcd3cf42d4028
Nick Jones <nickj_24@hotmail.com>
2022-06-14 16:47:04 +0100

    W7 D2 homework

---
+++ W7 homework/Day2/components/FilmButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FilmButton = () => {
+
+    return(
+
+    <form action="https://www.imdb.com/calendar/?region=gb">
+    <input type="submit" value="See Upcoming Films" />
+    </form>
+    )
+}
+
+export default FilmButton
